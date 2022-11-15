3841c1c85732753b733b85612d9dc12d0b88021e
RostoRM <92208542+RostoRM@users.noreply.github.com>
2022-11-15 16:14:47 +0400

    adding final project api

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  // connect to your own database here:
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    user: 'rostorm',
+    password: 'test',
+    database: 'smart-brain',
+  },
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+
+app.get('/', (req, res) => {
+  res.send('Success');
+});
+
+app.post('/signin', signin.handleSignin(db, bcrypt));
+
+app.post('/register', register.handleRegister(db, bcrypt));
+
+app.get('/profile/:id', profile.handleProfileGet(db));
+
+app.put('/image', image.handleImage(db));
+
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(3000, () => {
+  console.log('Everything is working on 3000, keep going');
+});
