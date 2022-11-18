3555c7db8d728efad1acd2c2f2ec34c2df245311
RostoRM <92208542+RostoRM@users.noreply.github.com>
2022-11-18 19:45:30 +0400

    adding final project api changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = knex({
     host: 'dpg-cdrphu94rebbgsh3sco0-a',
     port: 5432,
     user: 'rostorm',
-    password: 'password',
+    password: 'iUdLbphNIggDIkuXfMN8TJSbUypmDIpg',
     database: 'smartbrain_jycq',
   },
 });
